62c13377bdff282278e8e2e405fd92a38a70a4a7
Benaya Trabelsi <benaya7@gmail.com>
2024-05-22 11:30:14 +0300

    added README.md

--- src/main/java/org/benaya/learn/polyglotwithvenv/constants/PythonScripts.java
+++ src/main/java/org/benaya/learn/polyglotwithvenv/constants/PythonScripts.java
@@ -11,13 +11,4 @@ public class PythonScripts {
             def is_list(obj):
                 return isinstance(obj, list)
             """;
-//    public static final String CHAT_SCRIPT = """
-//            from transformers import AutoModelForCausalLM, AutoTokenizer
-//            model = AutoModelForCausalLM.from_pretrained("dicta-il/dictalm2.0-instruct-GPTQ", device_map=device)
-//            tokenizer = AutoTokenizer.from_pretrained("dicta-il/dictalm2.0-instruct-GPTQ")
-//            def generate_response(user_input):
-//                input_ids = tokenizer.encode(user_input, return_tensors='pt')
-//                generated_ids = model.generate(encoded, max_new_tokens=50, do_sample=True)
-//                return tokenizer.batch_decode(generated_ids)
-//            """;
 }
